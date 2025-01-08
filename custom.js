1cf9867c5541815c6f0904bc881bab0f0c8420b8
MacAlistair1 <lamichhaneaj@gmail.com>
2025-01-08 16:44:51 +0545

    minor

--- custom.js
+++ custom.js
@@ -11,27 +11,41 @@ function scrollToSection(sectionId) {
 let footerYear = document.getElementById("year");
 footerYear.innerHTML = new Date().getFullYear();
 
-// let topSection = document.getElementById("sec-9249");
-// topSection.style.backgroundImage = "url('./assets/gallery/9-min.png')";
-
 let headerVideo = document.getElementById("header-video");
 let customVideo = document.getElementById("custom-video");
 let latestVideoATag = document.getElementById("latest-video");
 
 let videoId = "";
 
-fetch("https://macalistair1.github.io/scrap-data/latest_yt_video.txt")
+fetch("https://macalistair1.github.io/scrap-data/latest_yt_video.json")
   .then((response) => {
-    return response.text();
+    return response.json();
   })
   .then((data) => {
     if (data) {
-      let videoUrl = `https://www.youtube.com/embed/${data}?controls=0&autoplay=1`;
-      let videoWatchUrl = `https://www.youtube.com/watch?v=${data}`;
+      let videoUrl = `https://www.youtube.com/embed/${data.videoId}?controls=0&autoplay=1`;
+      let videoWatchUrl = `https://www.youtube.com/watch?v=${data.videoId}`;
       setTimeout(() => {
         customVideo.style.display = "none";
       }, 2000);
       headerVideo.setAttribute("src", videoUrl);
       latestVideoATag.setAttribute("href", videoWatchUrl);
+    }
+  });
+
+let galleryImage = document.getElementsByClassName("u-image-4")[0];
+
+fetch("https://macalistair1.github.io/scrap-data/reel.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    if (data) {
+      galleryImage.style.backgroundImage = `url(${data.image})`;
+      galleryImage.style.cursor = "pointer";
+
+      galleryImage.addEventListener("click", () => {
+        location.href = data.url;
+      });
     }
   });
